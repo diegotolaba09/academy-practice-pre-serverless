2685414ae416d29531a33f6c1ee76192e43ec33f
NaranjaX <naranjax@MAC-67733.local>
2022-08-02 16:47:00 -0300

    Feat(api/academy): Best practices

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const { dbConnect } = require("./config/mongo");
+const { dbConnect } = require("../config/mongo");
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1", require("./app/routes"));
+app.use("/api/v1", require("./routes"));
 
 dbConnect();
 
